7e8f6ba4341481f610989738e7af51b5d5ef17a1
StepanNaryshkov <stepan.naryshkov@gmail.com>
2018-11-25 12:47:57 +0200

    init

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main from '../screens/Main/Main';
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Main} />
+    </Switch>
+  </Router>
+);
+
+export default App;
---
+++ src/screens/Main/Main.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import styles from './styles.module.css';
+
+const Main = () => {
+  return (
+    <Fragment>
+      <h1>Lol</h1>
+    </Fragment>
+  );
+};
+export default Main;
