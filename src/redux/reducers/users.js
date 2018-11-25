7da848a56a4387aec2c244612a56ea4544512cfc
StepanNaryshkov <stepan.naryshkov@gmail.com>
2018-11-25 15:50:24 +0200

    added throttle for onchange

--- src/redux/reducers/users.js
+++ src/redux/reducers/users.js
@@ -1,11 +1,12 @@
 import CNST from '../constants/';
+import users from '../stores/users';
 
-export default function(state = [], action) {
+export default function(state = users, action) {
   switch (action.type) {
     case CNST.USERS.GET_USERS.FETCH:
       return { ...state, fetching: true };
     case CNST.USERS.GET_USERS.SUCCESS:
-      return { ...state, fetching: true, users: action.payload };
+      return { ...state, fetching: false, data: action.payload };
     case CNST.USERS.GET_USERS.ERROR:
       return { ...state, fetching: false };
     default:
--- src/screens/Main/component.js
+++ src/screens/Main/component.js
@@ -1,73 +1,68 @@
 import React from 'react';
+import throttle from 'lodash.throttle';
 import styles from './styles.module.css';
 import Loader from '../../components/Loader';
 
-// const user = {
-//   "login": "stepan",
-//   "id": 11852,
-//   "node_id": "MDQ6VXNlcjExODUy",
-//   "avatar_url": "https://avatars3.githubusercontent.com/u/11852?v=4",
-//   "gravatar_id": "",
-//   "url": "https://api.github.com/users/stepan",
-//   "html_url": "https://github.com/stepan",
-//   "followers_url": "https://api.github.com/users/stepan/followers",
-//   "following_url": "https://api.github.com/users/stepan/following{/other_user}",
-//   "gists_url": "https://api.github.com/users/stepan/gists{/gist_id}",
-//   "starred_url": "https://api.github.com/users/stepan/starred{/owner}{/repo}",
-//   "subscriptions_url": "https://api.github.com/users/stepan/subscriptions",
-//   "organizations_url": "https://api.github.com/users/stepan/orgs",
-//   "repos_url": "https://api.github.com/users/stepan/repos",
-//   "events_url": "https://api.github.com/users/stepan/events{/privacy}",
-//   "received_events_url": "https://api.github.com/users/stepan/received_events",
-//   "type": "User",
-//   "site_admin": false,
-//   "name": "Stepan Grigoryan",
-//   "company": null,
-//   "blog": "",
-//   "location": "San Francisco, CA",
-//   "email": null,
-//   "hireable": null,
-//   "bio": null,
-//   "public_repos": 10,
-//   "public_gists": 1,
-//   "followers": 16,
-//   "following": 9,
-//   "created_at": "2008-05-29T10:06:37Z",
-//   "updated_at": "2018-11-08T15:58:36Z"
-// };
+export default class Component extends React.Component {
+  constructor(props) {
+    super(props);
 
-const Component = ({ user, getUsers, fetching }) => {
-  return (
-    <div className={styles.container}>
-      <h1>Github API Navigator</h1>
-      <form>
-        <label htmlFor="input" className={styles.label}>
-          Search
-        </label>
-        <input
-          className={styles.input}
-          type="text"
-          id="input"
-          onChange={event => getUsers(event.target.value)}
-        />
-      </form>
-      {fetching && <Loader />}
-      {user && (
-        <ul>
-          <li className={styles.item}>
-            <img src={user.avatar_url} alt={user.name} height={40} width={40} />
-          </li>
-          <li className={styles.item}>
-            <a href={user.html_url}>{user.html_url}</a>
-          </li>
-          <li className={styles.item}>{user.name}</li>
-          <li className={styles.item}>{user.location}</li>
-          <li className={styles.item}>User ID: {user.id}</li>
-          <li className={styles.item}>Type: {user.type}</li>
-        </ul>
-      )}
-    </div>
-  );
-};
+    this.onChange = this.onChange.bind(this);
+    this.throttleOnChange = throttle(this.throttleOnChange.bind(this), 300);
+  }
 
-export default Component;
+  onChange(event) {
+    this.throttleOnChange(event.target.value);
+  }
+
+  throttleOnChange(value) {
+    if (value) {
+      this.props.getUsers(value);
+    }
+  }
+  render() {
+    const { fetching, user } = this.props;
+    return (
+      <div className={styles.container}>
+        <h1>Github API Navigator</h1>
+        <form>
+          <label htmlFor="input" className={styles.label}>
+            Search
+          </label>
+          <input
+            className={styles.input}
+            type="text"
+            id="input"
+            onChange={this.onChange}
+          />
+        </form>
+        {fetching && <Loader />}
+        {user && (
+          <ul>
+            <li className={styles.item}>
+              <img
+                src={user.avatar_url}
+                alt={user.name}
+                height={40}
+                width={40}
+              />
+            </li>
+            <li className={styles.item}>
+              <a href={user.html_url}>
+                {user.html_url || 'User link: not found'}
+              </a>
+            </li>
+            <li className={styles.item}>
+              {user.name || 'User name: not found'}
+            </li>
+            <li className={styles.item}>
+              {user.location || 'User name: not found'}
+            </li>
+            <li className={styles.item}>User ID: {user.id || 'not found'}</li>
+            <li className={styles.item}>Type: {user.type || 'not found'}</li>
+          </ul>
+        )}
+      </div>
+    );
+  }
+}
