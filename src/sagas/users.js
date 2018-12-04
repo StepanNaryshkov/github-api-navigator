6e6f69c916ccd6a215a7f9db47de257000c17c89
Stepan Naryshkov <stepan@omniaz.io>
2018-12-04 16:27:21 +0200

    added immer

--- src/sagas/users.js
+++ src/sagas/users.js
@@ -4,10 +4,7 @@ import api from '../config/api';
 
 export function* getUsers(props) {
   try {
-    const response = {
-      data: { id: 12 }
-    };
-    //const response = yield call(api.getUsers, props.payload);
+    const response = yield call(api.getUsers, props.payload);
     yield put({ type: CNST.USERS.GET_USERS.SUCCESS, payload: response.data });
   } catch (error) {
     yield put({ type: CNST.USERS.GET_USERS.ERROR, error });
